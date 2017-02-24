8f4e002bc291feee6efdd77cbe72183c8484bbab
aotoole <aotoole@fwparker.org>
2017-02-24 14:08:20 -0600

    Hmwk.

--- ch15Lab.py
+++ ch15Lab.py
@@ -2,5 +2,65 @@
 Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
 '''
 
+import re
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+file = open("dictionary.txt", "r")
+
+dictionary_list = []
+
+for line in file:
+    for words in line:
+        dictionary_list.append(words)
+file.close()
+
+print(dictionary_list)
+
+print('---Linear Search---')
+alice_file = open("AliceInWonderLand200.txt", "r")
+line_number = 0
+
+for line in alice_file:
+    words = split_line(line)
+    line_number += 1
+    for i in range(len(words)):
+        for j in range(len(dictionary_list)):
+            if words[i].lower() == dictionary_list[j].lower():
+                break
+            elif j == len(dictionary_list):
+                print(words[i],'on line', line_number, 'was not found.')
+
+alice_file.close()
 
 
+print('---Binary Search---')
+
+
+alice_file = open("AliceInWonderLand200.txt", "r")
+line_number = 0
+
+lower_bound = 0
+upper_bound = len(dictionary_list)-1
+
+found = False
+
+
+
+
+for line in alice_file:
+    words = split_line(line)
+    line_number += 1
+    for i in range(len(words)):
+        while lower_bound <= upper_bound and not found:
+            middle_position = (lower_bound + upper_bound) // 2
+            if dictionary_list[middle_position].lower() < words[i].lower():
+                lower_bound += middle_position + 1
+            elif dictionary_list[middle_position].lower() > words[i].lower():
+                upper_bound -= middle_position - 1
+            else:
+                found = True
+        if not found:
+            print(words[i],'on line', line_number, 'was not found.')
+
+alice_file.close()
--- ch15ProblemSet.py
+++ ch15ProblemSet.py
@@ -7,9 +7,52 @@ from class and from Ch15 of the textbook website
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.
 
+dictionary = open("dictionary.txt", "r")
+word_length = 0
+longest_word = ''
+
+for word in dictionary:
+    if len(word)-1 > word_length:
+        word_length = len(word)-1
+        longest_word = word
+    if len(word)-1 == word_length:
+        longest_word = word
+
+
+
+print(word_length)
+print(longest_word)
+
+
+
+
 #2.  (10pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
+
+
+import re
+def split_line(line):
+    # This function takes in a line of text and returns a list of words in the line
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+file1 = open("AliceInWonderLand.txt", "r")
+
+total = []
+letters = 0
+
+for line in file1:
+    word = split_line(line)
+    for i in range(len(word)):
+        total.append(word[i])
+    for j in range(len(word)):
+        letters += len(word[j])
+
+print('There are', len(total), 'words found in the text.')
+average = letters / len(total)
+print(average)
+
+
 
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
@@ -17,10 +60,32 @@ from class and from Ch15 of the textbook website
 # How many times does "Cat" occur?
 # How many times does "Cheshire" immediately followed by "Cat" occur?
 
+cat_total = 0
+cheshire_total = 0
+cheshire_cat_total = 0
+
+for i in range(len(total)):
+    if total[i].lower() == 'cat':
+        cat_total += 1
+    if total[i].lower() == 'cheshire':
+        cheshire_total += 1
+    if total[i].lower() == 'cheshire' and i < (len(total)-1):
+        if total[i + 1].lower() == 'cat':
+            cheshire_cat_total += 1
+
+print(cat_total)
+print(cheshire_total)
+print(cheshire_cat_total)
+
+
+
+
+
 #### OR #####
 
 #3  (13pts)Find the most frequently occurring
 # seven letter word in "AliceInWonderLand.txt"
+
 
 # Challenge problem (for fun).  What words appear in the text of "Alice in Wonderland" that DO NOT occur in "Alice Through the Looking Glass".  Make a list.  You can substitute this for any of the above problems.
 
