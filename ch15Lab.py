2ea05b82aa5669ee816b1e3cc97dbcc0f3d60b98
aotoole <aotoole@fwparker.org>
2017-02-24 14:56:17 -0600

    Hmwk.

--- ch15Lab.py
+++ ch15Lab.py
@@ -1,3 +1,4 @@
+
 '''
 Complete the chapter lab at http://programarcadegames.com/index.php?chapter=lab_spell_check
 '''
@@ -11,8 +12,7 @@ file = open("dictionary.txt", "r")
 dictionary_list = []
 
 for line in file:
-    for words in line:
-        dictionary_list.append(words)
+    dictionary_list.append(line.strip())
 file.close()
 
 print(dictionary_list)
@@ -20,20 +20,20 @@ print(dictionary_list)
 print('---Linear Search---')
 alice_file = open("AliceInWonderLand200.txt", "r")
 line_number = 0
-
+'''
 for line in alice_file:
     words = split_line(line)
     line_number += 1
-    for i in range(len(words)):
+    for word in words:
         for j in range(len(dictionary_list)):
-            if words[i].lower() == dictionary_list[j].lower():
+            if word.lower() == dictionary_list[j].lower():
                 break
-            elif j == len(dictionary_list):
-                print(words[i],'on line', line_number, 'was not found.')
+        else:
+            print(word,'on line', line_number, 'was not found.')
 
 alice_file.close()
 
-
+'''
 print('---Binary Search---')
 
 
@@ -45,22 +45,19 @@ upper_bound = len(dictionary_list)-1
 
 found = False
 
-
-
-
 for line in alice_file:
     words = split_line(line)
     line_number += 1
-    for i in range(len(words)):
+    for word in words:
         while lower_bound <= upper_bound and not found:
             middle_position = (lower_bound + upper_bound) // 2
-            if dictionary_list[middle_position].lower() < words[i].lower():
+            if dictionary_list[middle_position].lower() < word.lower():
                 lower_bound += middle_position + 1
-            elif dictionary_list[middle_position].lower() > words[i].lower():
+            elif dictionary_list[middle_position].lower() > word.lower():
                 upper_bound -= middle_position - 1
-            else:
+            elif dictionary_list[middle_position].lower() == word.lower():
                 found = True
-        if not found:
-            print(words[i],'on line', line_number, 'was not found.')
+            else:
+                print(word,'on line', line_number, 'was not found.')
 
 alice_file.close()
